1a8caf74ae9e0dbfc0e5a96f81a6ab4098bef0c6
GaoYang <GaoYangBenYang@outlook.com>
2024-02-22 16:12:29 +0800

    :rocket: jdk14 Switch表达式和Switch语法

--- Java/src/main/java/com/gaoyang/jdk17/Switch.java
+++ Java/src/main/java/com/gaoyang/jdk17/Switch.java
@@ -9,10 +9,14 @@ package com.gaoyang.jdk17;
  * 3.值返回：Switch表达式可以返回一个值，这使得它们可以被赋值给一个变量或者作为方法的返回值。
  * <p>
  * 4.新关键字：
- *          yield：在Switch表达式中，用于返回一个值。它替代了传统Switch语句中的break语句，并可以将结果直接返回给调用方。
- *          ->：箭头符号，用于分隔每个分支的条件和执行语句。它取代了传统Switch语句中的case和:符号的组合。
- *          ,：逗号，用于在一个分支中列举多个常量值，表示它们共享相同的执行语句。
- *          default：用于指定Switch表达式中的默认分支，当没有任何分支匹配时执行该分支的代码块。
+ * <p>
+ * yield：在Switch表达式中，用于返回一个值。它替代了传统Switch语句中的break语句，并可以将结果直接返回给调用方。
+ * <p>
+ * ->：箭头符号，用于分隔每个分支的条件和执行语句。它取代了传统Switch语句中的case和:符号的组合。
+ * <p>
+ * ,：逗号，用于在一个分支中列举多个常量值，表示它们共享相同的执行语句。
+ * <p>
+ * default：用于指定Switch表达式中的默认分支，当没有任何分支匹配时执行该分支的代码块。
  * <p>
  * 5.简化的语法：Switch表达式可以省略每个分支的"break"语句，从而减少了代码冗余。
  * <p>
@@ -24,7 +28,7 @@ public class Switch {
         int day = 11;
         //Switch表达式
         String dayName = switch (day) {
-            case 1,10,6 -> "Monday";
+            case 1, 10, 6 -> "Monday";
             case 2 -> "Tuesday";
             case 3 -> "Wednesday";
             case 4 -> "Thursday";
@@ -33,17 +37,17 @@ public class Switch {
                 var a = "asd";
                 var b = "sdd";
 
-                yield a+b;
+                yield a + b;
 
             }
         };
-        
+
         System.out.println("Day: " + dayName);
 
 
         //Switch语法
         switch (day) {
-            case 1,10,6 -> {
+            case 1, 10, 6 -> {
                 System.out.println("111");
             }
             case 2 -> {
@@ -61,12 +65,10 @@ public class Switch {
             default -> {
                 var a = "asd";
                 var b = "sdd";
-                System.out.println(a+b);
+                System.out.println(a + b);
 
             }
-        };
-
-
+        }
 
     }
 }
