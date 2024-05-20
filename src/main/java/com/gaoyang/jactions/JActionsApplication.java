245ebef2e44d58a34d5978249410bf04716d7258
GaoYang <GaoYangBenYang@outlook.com>
2024-05-20 09:14:07 +0800

    :tada: JActions初始化

---
+++ src/main/java/com/gaoyang/jactions/JActionsApplication.java
@@ -0,0 +1,13 @@
+package com.gaoyang.jactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JActionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JActionsApplication.class, args);
+    }
+
+}
